4c06343682f1409acd81ac6c06bf8b5f51ac4790
Shinkou Incorporated <135273047+FaithfulAC@users.noreply.github.com>
2025-01-08 19:11:41 -0800

    Create universal-hookmetamethod-bypass.lua

---
+++ universal-hookmetamethod-bypass.lua
@@ -0,0 +1,11 @@
+local settings = (...) or { -- not much to add here
+        Source = "ReplicatedFirst.",
+        LookThroughTables = true,
+        LookThroughUpvalues = true,
+}
+
+local data = {}
+
+if settings.LookThroughTables then
+
+end
